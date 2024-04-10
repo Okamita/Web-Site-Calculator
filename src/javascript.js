37bfbc2e1911aee9013fa98cecae890d70c17a4f
Okamita <simone.enselmi@gmail.com>
2024-04-10 15:31:11 +0200

    Add the AC functionality

--- src/javascript.js
+++ src/javascript.js
@@ -1,13 +1,12 @@
-
-
-
-
 let firstInput;
 let secondInput;
 let operation;
 
+//Add the functionality to all the operator buttons
 const operatorButtons = document.querySelectorAll(".operatorButton");
 
+/*Every button operator change the operator only when the first number is been
+added and the second number is not being added*/
 operatorButtons.forEach((button)=>{
     button.addEventListener("click", ()=>{
         if(firstInput !== undefined && secondInput === undefined){
@@ -17,10 +16,12 @@ operatorButtons.forEach((button)=>{
     });
 });
 
-
-
+//Add the functionality to all numbers buttons
 const numberButtons = document.querySelectorAll(".numberButton");
 
+/*Every numbered button change the content of the screen. The first input
+is added only when the operator is not clicked, if instead the operation is 
+available than it change only the second number.*/
 numberButtons.forEach((button)=>{
     button.addEventListener("click", ()=>{
         if(operation === undefined){
@@ -34,16 +35,32 @@ numberButtons.forEach((button)=>{
 });
 
 
+//Add the functionality to the AC button to empty the field calculator
+const acButton = document.querySelector("#acButton");
+
+//When we click the AC we reset the field and the variables of the numbers and operation
+acButton.addEventListener("click", ()=>{
+    firstInput = undefined;
+    secondInput = undefined;
+    operation = undefined;
+    setOperatorScreen(undefined);
+    setFirstNumberScreen(undefined);
+    setSecondNumberText(undefined);
+});
+
+//Set the operatorText content to the parameter
 function setOperatorScreen(content){
     const operator = document.querySelector("#operator");
     operator.textContent = content;
 }
 
+//Set the first number text content to the parameter
 function setFirstNumberScreen(content){
     const firstNumberText = document.querySelector("#firstNumber");
     firstNumberText.textContent = content;
 }
 
+//Set the second number text  content to the parameter
 function setSecondNumberText(content){
     const secondNumberText = document.querySelector("#secondNumber");
     secondNumberText.textContent = content;
