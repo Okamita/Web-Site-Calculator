c5cdb3fcb3e9addd4cab3348df0bbff81f3fafd3
Okamita <simone.enselmi@gmail.com>
2024-04-10 15:23:16 +0200

    Make the buttons change the content of the blank field

--- src/javascript.js
+++ src/javascript.js
@@ -1,21 +1,63 @@
+
+
+
+
 let firstInput;
 let secondInput;
+let operation;
 
-const operator = {
+const operatorButtons = document.querySelectorAll(".operatorButton");
+
+operatorButtons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        if(firstInput !== undefined && secondInput === undefined){
+            operation = button.textContent;
+            setOperatorScreen(operation); 
+        }
+    });
+});
+
+
+
+const numberButtons = document.querySelectorAll(".numberButton");
+
+numberButtons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        if(operation === undefined){
+            firstInput = button.textContent;
+            setFirstNumberScreen(firstInput); 
+        }else{
+            secondInput = button.textContent;
+            setSecondNumberText(secondInput); 
+        }
+    });
+});
+
+
+function setOperatorScreen(content){
+    const operator = document.querySelector("#operator");
+    operator.textContent = content;
+}
+
+function setFirstNumberScreen(content){
+    const firstNumberText = document.querySelector("#firstNumber");
+    firstNumberText.textContent = content;
+}
+
+function setSecondNumberText(content){
+    const secondNumberText = document.querySelector("#secondNumber");
+    secondNumberText.textContent = content;
+}
+
+
+//Operations
+
+const operators = {
     add : "add",
     multiply : "multiply",
     divide : "divide",
 }
 
-
-
-
-function add(a, b) {return a + b};
-
-function multiply(a, b)  {return a * b};
-
-function divide(a, b)  {return a / b};
-   
 function operate(a, b, operator){
     switch(operator){
         case "add": 
@@ -29,3 +71,10 @@ function operate(a, b, operator){
             break
     }
 }
+
+function add(a, b) {return a + b};
+
+function multiply(a, b)  {return a * b};
+
+function divide(a, b)  {return a / b};
+   
