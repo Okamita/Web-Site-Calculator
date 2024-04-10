aa660ec79789d28f633ca1999abf8a7a49104152
Okamita <simone.enselmi@gmail.com>
2024-04-10 10:42:11 +0200

    Create main functions for the calculator

---
+++ src/javascript.js
@@ -0,0 +1,18 @@
+let firstInput;
+let secondInput;
+const operator = {
+    add : "add",
+    multiply : "multiply",
+    divide : "divide",
+}
+
+
+
+
+
+function add(a, b) {return a + b};
+
+function multiply(a, b)  {return a * b};
+
+function divide(a, b)  {return a / b};
+   
